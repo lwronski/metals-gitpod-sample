4e9efebcd47fe4f7b133e9029adfba38f732fbab
Łukasz Wroński <lukaszwronski98@gmail.com>
2022-01-24 15:34:09 +0100

    test

---
+++ Main.scala
@@ -0,0 +1,8 @@
+// using scala "2.12.8"
+
+object Main {
+
+    def main(args: Array[String]) = {
+        println("Hello world!")
+    }
+}
--- gitpod_command.sh
+++ gitpod_command.sh
@@ -1,9 +1,11 @@
+
 #!/bin/bash
 
 METALS_DIR="$GITPOD_REPO_ROOT/.metals"
 APPS_DIR="$METALS_DIR/apps"
+COURSIER_CACHE=".cache"
 
 mkdir -p $APPS_DIR
 
 export PATH=$PATH:/usr/local/openjdk-8/bin:$APPS_DIR
-bloop about
+export COURSIER_CACHE=$COURSIER_CACHE
